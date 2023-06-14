c5de506a91a1cbed00296743c6b9ba98cd67ff63
Felipe <felipefggw@gmail.com>
2023-06-14 19:12:18 -0300

    Aula 2.7 Criando um controller com Spring MVC alteracao de comentario

--- algafood-api/src/main/java/com/algaWorks/algafood/MeuPrimeiroController.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/MeuPrimeiroController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller //anotacao 
+@Controller //Anotacao do REST controller 
 public class MeuPrimeiroController {
 	
 	@GetMapping("/hello") //caminho 
