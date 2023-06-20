19617dd56831581736fd237cc3b1ff0d1262d78a
Felipe <felipefggw@gmail.com>
2023-06-20 19:33:29 -0300

    2.14. Conhecendo os pontos de injeção e a anotação @Autowired

--- algafood-api/src/main/java/com/algaWorks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/di/notificacao/NotificadorEmail.java
@@ -4,31 +4,13 @@ import org.springframework.stereotype.Component;
 
 import com.algaWorks.algafood.di.modelo.Cliente;
 
+@Component
 public class NotificadorEmail implements Notificador {
-	
-	private boolean caixaAlta;
-	private String hostServidorSmtp;
-	
-	
-	public NotificadorEmail(String hosServidorSmp) {
-		System.out.println("NotificadorEmail");
-		this.hostServidorSmtp = hostServidorSmtp;
-		System.out.println("NotificadorEmail");
-	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		if(this.caixaAlta) {
-			mensagem = mensagem.toUpperCase();
+		System.out.printf("Notificando %s através do e-mail %s: %s\n",
+				cliente.getNome(),cliente.getEmail(), mensagem);
 		}
-		
-		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n",
-				cliente.getNome(),cliente.getEmail(), this.hostServidorSmtp, mensagem);
-		}
-	
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
 	}
-		
-}
 
--- algafood-api/src/main/java/com/algaWorks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/di/service/AtivacaoClienteService.java
@@ -1,27 +1,35 @@
 package com.algaWorks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaWorks.algafood.di.modelo.Cliente;
 import com.algaWorks.algafood.di.notificacao.Notificador;
 import com.algaWorks.algafood.di.notificacao.NotificadorEmail;
 
-
-
+@Component
 public class AtivacaoClienteService {
 	
+	@Autowired
 	private Notificador notificador;
 	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService " + notificador);
-	}
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+//	
+//	public AtivacaoClienteService(String qualquer) {
+//		
+//	}
 
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		notificador.notificar(cliente,"Seu cadastro no sistema está ativo");
 	}
-	
+
+	public void setNotificador(Notificador notificador) {
+		this.notificador = notificador;
+	}
+		
 }
