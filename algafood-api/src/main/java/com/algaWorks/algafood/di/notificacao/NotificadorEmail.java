ef6672577e89072bb0f5f2e137949d7377b8641d
Felipe <felipefggw@gmail.com>
2023-06-29 18:49:12 -0300

    Propriedades customizadas

--- algafood-api/src/main/java/com/algaWorks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaWorks.algafood.di.notificacao;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -8,19 +9,31 @@ import org.springframework.stereotype.Component;
 import com.algaWorks.algafood.di.modelo.Cliente;
 
 
-@Profile("prod")
+//@Profile("prod")
 //@Qualifier("normal")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
 	
+	@Value("${notificador.emai.host-servidor}")
+	private String host;
+	
+	@Value("${notificador.email.porta-servidor}")
+	private int porta;
+	
 //	public NotificadorEmail() {
 //		System.out.println("Notificador Email Real");
 //	}
 	
+	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Host " + host);
+		System.out.println("Porta " + porta);
 		System.out.printf("Notificando %s através do e-mail %s: %s\n",
 				cliente.getNome(),cliente.getEmail(), mensagem);
 		}
+	
+	
+	
 	}
