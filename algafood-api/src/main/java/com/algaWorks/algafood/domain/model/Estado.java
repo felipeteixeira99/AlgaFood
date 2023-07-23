8ba99c360f79b4776ca64faecc94dcbd96af03d4
Felipe Teixeira <felipefggw@gmail.com>
2023-07-23 13:52:47 -0300

    Criacao da classe Estado

---
+++ algafood-api/src/main/java/com/algaWorks/algafood/domain/model/Estado.java
@@ -0,0 +1,28 @@
+package com.algaWorks.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "tabEstado")
+
+public class Estado {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+}
