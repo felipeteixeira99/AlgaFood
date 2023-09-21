839ece4d855f7fc3249716aff37f0856d316d0cb
Felipe <felipefggw@gmail.com>
2023-09-20 21:52:24 -0300

    atualizacao no import sql

--- algafood-api/src/main/java/com/algaWorks/algafood/infrastructure/repository/CidadeImpl.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/infrastructure/repository/CidadeImpl.java
@@ -40,5 +40,4 @@ public class CidadeImpl implements CidadeRepository{
 		cidade = buscarPorId(cidade.getId());
 		gerenciador.remove(cidade);
 	}
-
 }
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,24 +1,24 @@
-insert into cozinha (id, nome) values (1, 'Tailandesa');
-insert into cozinha (id, nome) values (2, 'Indiana');
+--insert into cozinha (id, nome) values (1, 'Tailandesa');
+--insert into cozinha (id, nome) values (2, 'Indiana');
 
-insert into restaurante(id,nome,taxa_frete, cozinha_id) values (1,'Burguer King', 10, 1);
-insert into restaurante(nome,taxa_frete, cozinha_id) values ('Girafas', 9.50, 1);
-insert into restaurante(nome,taxa_frete, cozinha_id) values ('SubWay', 9.50, 2);
+--insert into restaurante(id,nome,taxa_frete, cozinha_id) values (1,'Burguer King', 10, 1);
+--insert into restaurante(nome,taxa_frete, cozinha_id) values ('Girafas', 9.50, 1);
+--insert into restaurante(nome,taxa_frete, cozinha_id) values ('SubWay', 9.50, 2);
 
-insert into tab_forma_pagamento(id, nome) values (1, 'Dinheiro');
-insert into tab_forma_pagamento(nome) values ('Credito');
-insert into tab_forma_pagamento(nome) values ('Debito');
-insert into tab_forma_pagamento(nome) values ('Pix');
+--insert into tab_forma_pagamento(id, nome) values (1, 'Dinheiro');
+--insert into tab_forma_pagamento(nome) values ('Credito');
+--insert into tab_forma_pagamento(nome) values ('Debito');
+--insert into tab_forma_pagamento(nome) values ('Pix');
 
-insert into tab_estado(id, nome) values (1,'Goias');
-insert into tab_estado(nome) values ('Rio De Janeiro');
-insert into tab_estado(nome) values ('Ceara');
+--insert into tab_estado(id, nome) values (1,'Goias');
+--insert into tab_estado(nome) values ('Rio De Janeiro');
+--insert into tab_estado(nome) values ('Ceara');
 
-insert into tab_cidade(id, nome, estado_id) values (1, 'Goiania', 1);
-insert into tab_cidade(nome, estado_id) values ('Aquiraz', 2);
-insert into tab_cidade(nome, estado_id) values ('Anapolis', 2);
+--insert into tab_cidade(id, nome, estado_id) values (1, 'Goiania', 1);
+--insert into tab_cidade(nome, estado_id) values ('Aquiraz', 2);
+--insert into tab_cidade(nome, estado_id) values ('Anapolis', 2);
 
-insert into tab_permissoes (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
-insert into tab_permissoes (nome, descricao) values ('EDITAR_COZINHA', 'Permite editar cozinhas');
-insert into tab_permissoes (nome, descricao) values ('ADICIONAR_COZINHA', 'Permite editar cozinhas');
+--insert into tab_permissoes (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
+--insert into tab_permissoes (nome, descricao) values ('EDITAR_COZINHA', 'Permite editar cozinhas');
+--insert into tab_permissoes (nome, descricao) values ('ADICIONAR_COZINHA', 'Permite editar cozinhas');
 
