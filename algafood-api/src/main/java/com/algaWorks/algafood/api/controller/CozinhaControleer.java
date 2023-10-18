937e9d3ad2c00ea04fdd2ec0c21d9fa048b48fe0
Felipe <felipefggw@gmail.com>
2023-10-17 23:41:06 -0300

    subindo controoler cozinha

--- algafood-api/src/main/java/com/algaWorks/algafood/api/controller/CozinhaControleer.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/api/controller/CozinhaControleer.java
@@ -1,13 +1,29 @@
 package com.algaWorks.algafood.api.controller;
 
 import java.util.List;
-import com.algaWorks.algafood.domain.model.Cozinha;
 
-//public class CozinhaControleer {
-//	
-////	private CozinhaRepository
-////	
-////	public List<Cozinha> listar(){
-////		System.out.println("teste");
-////	}
-////}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaWorks.algafood.domain.model.Cozinha;
+import com.algaWorks.algafood.domain.repository.CozinhaRepository;
+
+@RestController
+//@Controller
+//@ResponseBody
+@RequestMapping("/cozinhas")
+
+public class CozinhaControleer {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar(){
+		return cozinhaRepository.buscarTodos();
+	}
+}
