5460192b548af7d31b9c3ccc3c3cde05384c2ecf
Felipe <felipefggw@gmail.com>
2023-11-26 22:25:14 -0300

    Implementação codigos HTTP

--- algafood-api/src/main/java/com/algaWorks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/api/controller/CozinhaController.java
@@ -3,12 +3,15 @@ package com.algaWorks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaWorks.algafood.domain.model.Cozinha;
@@ -38,10 +41,16 @@ public class CozinhaController {
 //		return cozinhaRepository.buscarTodos();
 //	}
 
+//	@ResponseStatus(HttpStatus.CREATED)
 	@GetMapping("/{cozinhaid}")
-	public Cozinha buscar(@PathVariable() Long cozinhaid) {
-		return cozinhaRepository.buscarPorId(cozinhaid);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha  cozinha = cozinhaRepository.buscarPorId(cozinhaId);
+		
+//		return ResponseEntity.status(HttpStatus.OK).build();
+		return ResponseEntity.ok();
+		
 	}
+
 	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public CozinhasXmlWrapper listarXml(){
@@ -49,3 +58,21 @@ public class CozinhaController {
 		
 	}
 }
+
+
+
+
+////@ResponseStatus(HttpStatus.CREATED)
+//@GetMapping("/{cozinhaid}")
+//public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+//	Cozinha  cozinha = cozinhaRepository.buscarPorId(cozinhaId);
+//	return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//	
+//}
+//
+//
+//@ResponseStatus(HttpStatus.CREATED)
+//@GetMapping("/{cozinhaid}")
+//public Cozinha buscar(@PathVariable() Long cozinhaid) {
+//	return cozinhaRepository.buscarPorId(cozinhaid);
+//}
