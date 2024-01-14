a6dba2dd1f7c0a9dcd495521a829f3a9c329621d
Felipe <felipefggw@gmail.com>
2024-01-13 22:44:49 -0300

    Exclusão - Criação do Controlador Cozinha

--- algafood-api/src/main/java/com/algaWorks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/api/controller/CozinhaController.java
@@ -1,83 +1,33 @@
 package com.algaWorks.algafood.api.controller;
 
-import java.net.http.HttpHeaders;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaWorks.algafood.domain.model.Cozinha;
-import com.algaWorks.algafood.domain.model.CozinhasXmlWrapper;
 import com.algaWorks.algafood.domain.repository.CozinhaRepository;
 
-@RestController
-//@Controller
-//@ResponseBody
-@RequestMapping(value = "/cozinhas")//, produces = MediaType.APPLICATION_JSON_VALUE)
+//Modelagem Rest API
+//Essa classe serve para que todas as requisições com a URI /cozinhas caiam nesse controlador
 
+
+//@Controller //Componente spring do tipo controlador
+//@ResponseBody //
+@RestController //Possui o Controller e o ResponseBody
+@RequestMapping("/cozinhas") //Mapeamento do controlador responsavel pelas as requisições especificando a URI desse controlador
 public class CozinhaController {
 	
-	@Autowired
-	private CozinhaRepository cozinhaRepository;
+	@Autowired  
+	private CozinhaRepository cozinhaRepository; //variavel de instancia que implementa a interface de Cozinha
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Cozinha> listar1(){
-		System.out.println("LISTAR 1");
+	@GetMapping //Significa que requisições com o verbo HTTP Get irão cair nesse metodo
+	public List<Cozinha> listarCozinha(){  //metodo que recebe e retorna a listagem de cozinhas salvas no banco
 		return cozinhaRepository.buscarTodos();
 	}
-	
-	
-//	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-//	public List<Cozinha> listar2(){
-//		System.out.println("LISTAR 2");
-//		return cozinhaRepository.buscarTodos();
-//	}
 
-	/*
-	 * @ResponseStatus(HttpStatus.CREATED)
-	 * 
-	 * @GetMapping("/{cozinhaId}") public ResponseEntity<Cozinha>
-	 * buscar(@PathVariable Long cozinhaId) { Cozinha cozinha =
-	 * cozinhaRepository.buscarPorId(cozinhaId); // return
-	 * ResponseEntity.status(HttpStatus.OK).body(cozinha); //retorna a requisição
-	 * com o corpo da resposta // return
-	 * ResponseEntity.status(HttpStatus.OK).build(); //retorna a requisição sem o
-	 * corpo da resposta // return ResponseEntity.ok(cozinha); //retorna a
-	 * requisição com o corpo da resposta // return ResponseEntity.ok(cozinha);
-	 * 
-	 * org.springframework.http.HttpHeaders headers = new
-	 * org.springframework.http.HttpHeaders();
-	 * headers.add(org.springframework.http.HttpHeaders.LOCATION,
-	 * "http://api.algafood.local:8080/cozinhas");
-	 * 
-	 * return ResponseEntity .status(HttpStatus.FOUND) .headers(headers) .build(); }
-	 */
-	
-
-	@GetMapping
-	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId){
-		Cozinha cozinha = cozinhaRepository.buscarPorId(cozinhaId);
-		
-		if (cozinha != null) {
-			return ResponseEntity.ok(cozinha);
-		}
-		//return ResponseEntity<Cozinha.status(HttpStatus.NOT_FOUND);
-	   return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-	}
-	 
-	
-	
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml(){
-		return new CozinhasXmlWrapper(cozinhaRepository.buscarTodos());
-		
-	}
 }
-
