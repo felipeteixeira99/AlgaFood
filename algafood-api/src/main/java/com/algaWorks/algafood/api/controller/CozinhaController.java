05cbbd0ce53b84595247351521f6e5b2d576e0d5
Felipe <felipefggw@gmail.com>
2024-01-15 22:54:28 -0300

    Manipulando a resposta HTTP com ResponseEntity

--- algafood-api/src/main/java/com/algaWorks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaWorks/algafood/api/controller/CozinhaController.java
@@ -3,12 +3,16 @@ package com.algaWorks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaWorks.algafood.domain.model.Cozinha;
@@ -55,10 +59,30 @@ public class CozinhaController {
 	
 	//Retorna uma cozinha conforme ID passado no parametro
 	//A anotação pathvariable atribui a variavel passada no getmapping para o id, e o mesmo é retornado no fim do metodo
-	@GetMapping("/{cozinhaId}")
-	public Cozinha buscarCozinhaId(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscarPorId(cozinhaId);
-	}
+	
+	//@ResponseStatus(HttpStatus.CREATED) //Alterar a resposta HTTP
+//	@GetMapping("/{cozinhaId}")
+//	public Cozinha buscarCozinhaId(@PathVariable Long cozinhaId) {
+//		return cozinhaRepository.buscarPorId(cozinhaId);
+//	}
+	 
+	 	//Customizando a resposta http e solicitando que ela retorne um corpo da resposta do tipo cozinha
+		@GetMapping("/{cozinhaId}")
+		public ResponseEntity <Cozinha> buscarCozinhaId(@PathVariable Long cozinhaId) {
+			Cozinha cozinha = cozinhaRepository.buscarPorId(cozinhaId);
+			
+//			return ResponseEntity.status(HttpStatus.OK).body(cozinha); //Retorna a resposta HTTP com um corpo
+//			return ResponseEntity.status(HttpStatus.OK).build(); //Retorna uma resposta HTTp, porem sem um corpo
+//			return ResponseEntity.ok(cozinha);  //Mesma coisa da linha acima, porem com menos codigo
+			
+			HttpHeaders headers = new HttpHeaders();
+			headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+			
+			return ResponseEntity
+					.status(HttpStatus.FOUND)
+					.headers(headers)
+					.build();
+		}
 		
 
 }
